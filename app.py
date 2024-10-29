1802346da3dc15bcd8696b6ca24b5b71b0f093f4
Amarildo <amarildozj@gmail.com>
2024-10-29 00:53:33 -0300

    Feature - Mudança em estrutura de projeto, para geração de arquivos de modelos e criação de endpoints para recomendação rápida ou completa

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import io
+import os
 import pandas as pd
-from flask import Flask, request, jsonify, make_response
-from entidades.dados import ManipulacaoCSV
-from entidades.KNN import AlgoritmoKNN
+from flask import Flask, request, jsonify
+from entidades.tratamentoDados import ManipulacaoCSV
+from entidades.treinamento import AlgoritmoKNN
+from config import  configuration
 
 app = Flask(__name__)
 
@@ -11,8 +13,8 @@ def Inicial():
     print("Teste")
 
     
-@app.route("/treinamentoRapido", methods=['POST'])
-def treinamento():
+@app.route("/treinamentoAlgoritmo", methods=['POST'])
+def Treinamento():
     try:
         if 'file' not in request.files:
             return jsonify({'Erro': 'Nenhum arquivo enviado'}), 400
@@ -27,8 +29,8 @@ def treinamento():
         csv_data = pd.read_csv(io.StringIO(file.stream.read().decode('ISO-8859-1')), delimiter=';')
         num_pca = request.form.get('NumPca')
         qtde_recomendacao = request.form.get('QtdeRecomendacao')
-
-        if not num_pca or not qtde_recomendacao:
+        
+        if not num_pca:
             return jsonify({'Erro': 'Parâmetro NumPca é obrigatórios'}), 400
         
         if not qtde_recomendacao:
@@ -43,6 +45,7 @@ def treinamento():
             qtde_recomendacao = int(qtde_recomendacao)
         except ValueError:
             return jsonify({'Erro': 'NumPca e QtdeRecomendacao devem ser números inteiros'}), 400
+        
         ManipulacaoDados = ManipulacaoCSV(csv_data)
         response, message = ManipulacaoDados.validarDadosCSV()
         if not response:
@@ -51,11 +54,45 @@ def treinamento():
         response, message, dataSet = ManipulacaoDados.tratamentoCSV()
         if not response:
             return jsonify({'Mensagem': message}), 400
+        
         KNN = AlgoritmoKNN(dataSet, num_pca, qtde_recomendacao)
-        KNN.treinamentoKNN()
-        recomendacao = KNN.recomendarTodosProdutos()
-
-        return jsonify({'Mensagem':recomendacao}), 200
+        response,message = KNN.treinamentoKNN()
+        
+        if response == 400:
+            return jsonify({'Erro': f'Ocorreu um erro: {message}'}), response
+        return jsonify({'Mensagem':'Seu algoritmo foi treinado com sucesso.'}), 200
     except Exception as error:
         return jsonify({'Erro': f'Ocorreu um erro: {error}'}), 500
 
+
+
+@app.route("/recomendacaoItem/<int:CodigoProduto>", methods=['GET'])
+def RecomendacaoItem(CodigoProduto):
+    try:
+        recomendador = AlgoritmoKNN('','','')
+        status, resultado = recomendador.RecomendarProdutosPorCodigo(CodigoProduto)
+        if status == 200:
+            return jsonify({'ProdutosRecomendados': resultado}), 200
+        else:
+            return jsonify({'Erro': resultado}), 400
+    except Exception as error:
+        return jsonify({'Erro': f'Ocorreu um erro: {error}'}), 500
+
+
+
+
+@app.route("/recomendacaoTodosItens", methods=['GET'])
+def RecomendacaoTodosItens():
+    try:
+        recomendador = AlgoritmoKNN('','','')
+        status, resultado,porcentagem_iguais = recomendador.RecomendarTodosProdutos()
+        if status == 200:
+            return jsonify({'PorcAcertoSecaoDepto':porcentagem_iguais,'ProdutosRecomendados': resultado}), 200
+        else:
+            return jsonify({'Erro': resultado}), 400
+    except Exception as error:
+        return jsonify({'Erro': f'Ocorreu um erro: {error}'}), 500
+    
+    
+if __name__ == '__main__':
+    app.run(host=configuration.ip, port=configuration.porta)
---
+++ config/configuration.py
@@ -0,0 +1,45 @@
+from configparser import ConfigParser
+import os
+
+# Caminho absoluto para o arquivo de configuração
+config_file = r'C:\Users\amjun\Desktop\Catolica\TCC\TCCPrivado\config\config.ini'
+
+# Inicializa o ConfigParser
+conf_obj = ConfigParser()
+
+# Verifica se o arquivo existe
+if os.path.exists(config_file):
+    conf_obj.read(config_file)
+else:
+    raise FileNotFoundError(f"Arquivo de configuração não encontrado: {config_file}")
+
+try:
+    # Acessando a seção 'Parametros'
+    if 'Parametros' in conf_obj:
+        config = conf_obj['Parametros']
+        UrlPastaModelos = config.get('UrlPastaModelos', None)
+        UrlPastaParametros = config.get('UrlPastaParametros', None)
+        UrlPastaDataSet = config.get('UrlPastaDataSet', None)
+        ip = config.get('ip', None)
+        porta = config.get('porta', None)
+        
+        if UrlPastaModelos is None:
+            raise ValueError("Chave 'UrlPastaModelos' não encontrada na seção [Parametros].")
+        
+        if UrlPastaParametros is None:
+            raise ValueError("Chave 'UrlPastaParametros' não encontrada na seção [Parametros].")
+        
+        if UrlPastaDataSet is None:
+            raise ValueError("Chave 'UrlPastaDataSet' não encontrada na seção [Parametros].")
+        
+        if ip is None:
+            raise ValueError("Chave 'ip' não encontrada na seção [Parametros].")
+
+        if porta is None:
+            raise ValueError("Chave 'porta' não encontrada na seção [Parametros].")
+    else:
+        raise ValueError("Seção 'Parametros' não encontrada no arquivo de configuração.")
+
+
+except Exception as e:
+    print(f"Erro ao ler o arquivo de configuração: {e}")
